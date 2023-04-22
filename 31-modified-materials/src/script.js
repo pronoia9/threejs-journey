2f444b57d9d89519036a433e16a8e0a88d971928
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-22 16:53:35 +0300

    add 31-modified-materials

---
+++ 31-modified-materials/src/script.js
@@ -0,0 +1,171 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as dat from 'dat.gui';
+
+const DEBUG = true;
+
+/***   Base   ***/
+// Debug
+const gui = new dat.GUI();
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+// Scene
+const scene = new THREE.Scene();
+
+/***   Loaders   ***/
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+/***   Environment map   ***/
+const environmentMap = cubeTextureLoader.load(
+  Array.from({ length: 6 }, (v, i) => `/textures/environmentMaps/0/${['px', 'nx', 'py', 'ny', 'pz', 'nz'][i]}.jpg`)
+);
+environmentMap.encoding = THREE.sRGBEncoding;
+scene.background = environmentMap;
+scene.environment = environmentMap;
+
+/***   Material   ***/
+// Textures
+const mapTexture = textureLoader.load('/models/LeePerrySmith/color.jpg');
+mapTexture.encoding = THREE.sRGBEncoding;
+const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg');
+// Material
+const material = new THREE.MeshStandardMaterial({ map: mapTexture, normalMap: normalTexture, wireframe: false });
+const depthMaterial = new THREE.MeshDepthMaterial({ depthPacking: THREE.RGBADepthPacking });
+// Hook
+const customUniforms = { uTime: { value: 0 } };
+material.onBeforeCompile = (shader) => {
+  DEBUG && console.log('material', shader);
+  DEBUG && console.log('material', shader.uniforms);
+  shader.uniforms.uTime = customUniforms.uTime;
+  shader.vertexShader = shader.vertexShader
+    .replace(
+      '#include <common>',
+      `#include <common>
+       uniform float uTime;
+       mat2 rotate2d(float _angle) {
+         return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+       }`
+    )
+    .replace(
+      '#include <beginnormal_vertex>',
+      `#include <beginnormal_vertex> 
+       float angle = - (position.y + uTime) * 0.9;
+       mat2 rotateMatrix = rotate2d(angle);
+       objectNormal.xz *= rotateMatrix;`
+    )
+    .replace(
+      '#include <begin_vertex>',
+      `#include <begin_vertex>
+       transformed.xz *= rotateMatrix;`
+    );
+};
+depthMaterial.onBeforeCompile = (shader) => {
+  DEBUG && console.log('depth material', shader);
+  shader.uniforms.uTime = customUniforms.uTime;
+  shader.vertexShader = shader.vertexShader
+    .replace(
+      `#include <common>`,
+      `#include <common>
+       uniform float uTime;
+       mat2 rotate2d(float _angle) {
+         return mat2(cos(_angle), - sin(_angle), sin(_angle), cos(_angle));
+       }`
+    )
+    .replace(
+      `#include <begin_vertex>`,
+      `#include <begin_vertex> 
+       float angle = - (position.y + uTime) * 0.9;
+       mat2 rotateMatrix = rotate2d(angle);
+       transformed.xz = transformed.xz * rotateMatrix;`
+    );
+};
+
+/***   Models   ***/
+gltfLoader.load('/models/LeePerrySmith/LeePerrySmith.glb', (gltf) => {
+  // Model
+  const mesh = gltf.scene.children[0];
+  mesh.rotation.y = Math.PI * 0.5;
+  mesh.material = material;
+  mesh.customDepthMaterial = depthMaterial;
+  scene.add(mesh);
+  // Update materials
+  updateAllMaterials();
+});
+
+// Test Plane
+const plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(15, 15, 15), new THREE.MeshStandardMaterial());
+(plane.position.y = -5), (plane.position.z = 5), (plane.rotation.y = Math.PI);
+scene.add(plane);
+
+/***   Lights   ***/
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.normalBias = 0.05;
+directionalLight.position.set(0.25, 2, -2.25);
+scene.add(directionalLight);
+
+/***   Sizes   ***/
+const sizes = { width: window.innerWidth, height: window.innerHeight };
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/***   Camera   ***/
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.set(4, 1, -4);
+scene.add(camera);
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/***   Renderer   ***/
+const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/***   Utils   ***/
+// Update all materials
+function updateAllMaterials() {
+  scene.traverse((child) => {
+    if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+      child.material.envMapIntensity = 1;
+      child.material.needsUpdate = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+}
+// Animate
+const clock = new THREE.Clock();
+(function tick() {
+  const elapsedTime = clock.getElapsedTime();
+  // Update material
+  customUniforms.uTime.value = elapsedTime;
+  // Update controls
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+})();
