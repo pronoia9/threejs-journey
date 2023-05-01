cecef5708d2d3fce6f95872737fabf3279f9bd65
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-01 23:09:19 +0300

    format

--- 43-r3f-drei/src/Experience.jsx
+++ 43-r3f-drei/src/Experience.jsx
@@ -1,33 +1,34 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { useThree, extend } from '@react-three/fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-extend({ OrbitControls })
+extend({ OrbitControls });
 
-export default function Experience()
-{
-    const { camera, gl } = useThree()
+export default function Experience() {
+  const { camera, gl } = useThree();
 
-    return <>
+  return (
+    <group>
+      <orbitControls args={[camera, gl.domElement]} />
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+      <group>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color='orange' />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+        <mesh position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color='mediumpurple' />
         </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+          <planeGeometry />
+          <meshStandardMaterial color='greenyellow' />
         </mesh>
-
-    </>
+      </group>
+    </group>
+  );
 }
