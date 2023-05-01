61e049ded9a422d159ee54c491de1ef4348243f9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-01 17:22:23 +0300

    add meshes

--- 42-first-r3f-application/src/App.jsx
+++ 42-first-r3f-application/src/App.jsx
@@ -1,8 +1,20 @@
+import { useFrame } from '@react-three/fiber';
+
 export default function App() {
   return (
-    <mesh>
-      <torusKnotGeometry />
-      <meshNormalMaterial />
-    </mesh>
+    <group>
+      <mesh position={[-2, 0, 0]}>
+        <sphereGeometry />
+        <meshBasicMaterial color='orange' />
+      </mesh>
+      <mesh position={[2, 0, 0]} rotation={[0, Math.PI * 0.25, 0]} scale={1.5}>
+        <boxGeometry />
+        <meshBasicMaterial color='purple' />
+      </mesh>
+      <mesh position={[0, -1, 0]} rotation={[-Math.PI * 0.5, 0, 0]} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color='green' />
+      </mesh>
+    </group>
   );
 }
