02ff57c286f1ee0e6bfa31c37e059d632e99e39c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-01 17:31:47 +0300

    animate cube

--- 42-first-r3f-application/src/App.jsx
+++ 42-first-r3f-application/src/App.jsx
@@ -1,13 +1,19 @@
+import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 export default function App() {
+  const cubeRef = useRef();
+  useFrame(() => {
+    cubeRef.current.rotation.y += 0.01;
+  });
+
   return (
     <group>
       <mesh position={[-2, 0, 0]}>
         <sphereGeometry />
         <meshBasicMaterial color='orange' />
       </mesh>
-      <mesh position={[2, 0, 0]} rotation={[0, Math.PI * 0.25, 0]} scale={1.5}>
+      <mesh position={[2, 0, 0]} rotation={[0, Math.PI * 0.25, 0]} scale={1.5} ref={cubeRef}>
         <boxGeometry />
         <meshBasicMaterial color='purple' />
       </mesh>
