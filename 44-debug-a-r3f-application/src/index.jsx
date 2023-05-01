35fcac149672ee529ad132ae16bff56d75f4eb88
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-02 01:47:06 +0300

    add strict mode

--- 44-debug-a-r3f-application/src/index.jsx
+++ 44-debug-a-r3f-application/src/index.jsx
@@ -1,19 +1,23 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+import './style.css';
+import Experience from './Experience.jsx';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
+  <StrictMode>
     <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
-        <Experience />
+      <Experience />
     </Canvas>
-)
+  </StrictMode>
+);
