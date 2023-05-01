5720efac8e57c6ace43f3fd1b6ae6df1ed1ca2b9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-02 02:26:03 +0300

    add visible

--- 44-debug-a-r3f-application/src/Experience.jsx
+++ 44-debug-a-r3f-application/src/Experience.jsx
@@ -5,10 +5,11 @@ import Cube from './Cube';
 import Sphere from './Sphere';
 
 export default function Experience() {
-  const { spherePosition, sphereScale, sphereColor } = useControls({
+  const { spherePosition, sphereScale, sphereColor, sphereVisibility } = useControls({
     spherePosition: { value: { x: -2, y: 0 }, step: 0.01, joystick: 'invertY' },
     sphereScale: 1,
     sphereColor: '#ff0000',
+    sphereVisibility: true,
   });
 
   return (
@@ -21,7 +22,12 @@ export default function Experience() {
       <ambientLight intensity={0.5} />
 
       {/* Objects */}
-      <Sphere position={[...Object.values(spherePosition), 0]} scale={sphereScale} color={sphereColor} />
+      <Sphere
+        position={[...Object.values(spherePosition), 0]}
+        scale={sphereScale}
+        color={sphereColor}
+        visible={sphereVisibility}
+      />
       <Cube position={[2, 0, 0]} scale={1.5} />
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
--- 44-debug-a-r3f-application/src/Sphere.jsx
+++ 44-debug-a-r3f-application/src/Sphere.jsx
@@ -1,6 +1,12 @@
-export default function Sphere({ position = [0, 0, 0], rotation = [0, 0, 0], scale = 1, color = 'orange' }) {
+export default function Sphere({
+  position = [0, 0, 0],
+  rotation = [0, 0, 0],
+  scale = 1,
+  color = 'orange',
+  visible,
+}) {
   return (
-    <mesh position={position} rotation={rotation} scale={scale}>
+    <mesh position={position} rotation={rotation} scale={scale} visible={visible}>
       <sphereGeometry />
       <meshStandardMaterial color={color} />
     </mesh>
