dcf102b132ae6dceefe4819d1e8877fa040655ea
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-01 17:36:49 +0300

    rotate both objects

--- 42-first-r3f-application/src/App.jsx
+++ 42-first-r3f-application/src/App.jsx
@@ -2,21 +2,26 @@ import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 export default function App() {
-  const cubeRef = useRef();
+  const cubeRef = useRef(),
+    objsGroupRef = useRef();
+
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta;
+    // objsGroupRef.current.rotation.y += delta;
   });
 
   return (
     <group>
-      <mesh position={[-2, 0, 0]}>
-        <sphereGeometry />
-        <meshBasicMaterial color='orange' />
-      </mesh>
-      <mesh position={[2, 0, 0]} rotation={[0, Math.PI * 0.25, 0]} scale={1.5} ref={cubeRef}>
-        <boxGeometry />
-        <meshBasicMaterial color='purple' />
-      </mesh>
+      <group ref={objsGroupRef}>
+        <mesh position={[-2, 0, 0]}>
+          <sphereGeometry />
+          <meshBasicMaterial color='orange' />
+        </mesh>
+        <mesh position={[2, 0, 0]} rotation={[0, Math.PI * 0.25, 0]} scale={1.5} ref={cubeRef}>
+          <boxGeometry />
+          <meshBasicMaterial color='purple' />
+        </mesh>
+      </group>
       <mesh position={[0, -1, 0]} rotation={[-Math.PI * 0.5, 0, 0]} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color='green' />
