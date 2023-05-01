197ce0639a9cbeb03a306b9aadfe6e84d79a77a3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-01 20:32:10 +0300

    finish

--- 42-first-r3f-application/src/index.jsx
+++ 42-first-r3f-application/src/index.jsx
@@ -1,5 +1,6 @@
 import './style.css';
 import ReactDOM from 'react-dom/client';
+import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 
 import App from './App';
@@ -9,6 +10,9 @@ const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <Canvas
     // orthographic
+    // flat
+    // dpr={[1, 2]}
+    // gl={{ antialias: false, toneMapping: THREE.CineonToneMapping, outputEncoding: THREE.LinearEncoding }}
     camera={{ fov: 45, /* zoom: 100, */ near: 0.1, far: 200, position: [3, 2, 6] }}
   >
     <App />
