82057c85ada6b7039617de818e32bfe5c4b86991
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-02 02:32:42 +0300

    add cube scale

--- 44-debug-a-r3f-application/src/Experience.jsx
+++ 44-debug-a-r3f-application/src/Experience.jsx
@@ -5,11 +5,14 @@ import Cube from './Cube';
 import Sphere from './Sphere';
 
 export default function Experience() {
-  const { spherePosition, sphereScale, sphereColor, sphereVisibility } = useControls({
-    spherePosition: { value: { x: -2, y: 0 }, step: 0.01, joystick: 'invertY' },
-    sphereScale: 1,
-    sphereColor: '#ff0000',
-    sphereVisibility: true,
+  const { sPosition, sScale, sColor, sVisibility } = useControls('sphere', {
+    sPosition: { value: { x: -2, y: 0 }, step: 0.01, joystick: 'invertY' },
+    sScale: 1,
+    sColor: '#ff0000',
+    sVisibility: true,
+  });
+  const { cScale } = useControls('cube', {
+    cScale: { value: 1.5, min: 0, max: 5, step: 0.01 },
   });
 
   return (
@@ -23,12 +26,12 @@ export default function Experience() {
 
       {/* Objects */}
       <Sphere
-        position={[...Object.values(spherePosition), 0]}
-        scale={sphereScale}
-        color={sphereColor}
-        visible={sphereVisibility}
+        position={[...Object.values(sPosition), 0]}
+        scale={sScale}
+        color={sColor}
+        visible={sVisibility}
       />
-      <Cube position={[2, 0, 0]} scale={1.5} />
+      <Cube position={[2, 0, 0]} scale={cScale} />
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshStandardMaterial color='greenyellow' />
