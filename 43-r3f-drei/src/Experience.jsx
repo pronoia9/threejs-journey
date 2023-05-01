03fb48ef774e23db0f7c0b00a1dc635922fa67a0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-01 23:27:01 +0300

    add orbit and transform controls

--- 43-r3f-drei/src/Experience.jsx
+++ 43-r3f-drei/src/Experience.jsx
@@ -1,19 +1,28 @@
+import { OrbitControls, TransformControls } from '@react-three/drei';
+
 export default function Experience() {
   return (
     <group>
+      {/* Controls */}
+      <OrbitControls />
+
+      {/* Lights */}
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
+      {/* Objects */}
       <group>
         <mesh position-x={-2}>
           <sphereGeometry />
           <meshStandardMaterial color='orange' />
         </mesh>
 
-        <mesh position-x={2} scale={1.5}>
-          <boxGeometry />
-          <meshStandardMaterial color='mediumpurple' />
-        </mesh>
+        <TransformControls position-x={2}>
+          <mesh scale={1.5}>
+            <boxGeometry />
+            <meshStandardMaterial color='mediumpurple' />
+          </mesh>
+        </TransformControls>
 
         <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
           <planeGeometry />
