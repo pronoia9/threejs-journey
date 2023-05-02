9a757edf2c580564b15454f9fc360565d8da76d0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-02 20:35:52 +0300

    add light helper

--- 45-environment-and-staging-with-r3f/src/Experience.jsx
+++ 45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,14 +1,18 @@
-import { useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import { useRef } from 'react';
+import { DirectionalLightHelper } from 'three';
+import { useFrame } from '@react-three/fiber';
+import { OrbitControls, useHelper } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
-  const cube = useRef();
+  const cube = useRef(),
+    dirLightRef = useRef();
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
   });
+
+  useHelper(dirLightRef, DirectionalLightHelper, 1);
 
   return (
     <>
@@ -16,7 +20,7 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} ref={dirLightRef} />
       <ambientLight intensity={0.5} />
 
       <mesh position-x={-2}>
