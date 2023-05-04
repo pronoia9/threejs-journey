33f2c61cccd5ca0b5e32542568c9e5de0dac3271
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-04 15:14:22 +0300

    second breakfast -pippin

--- 46-load-models-with-r3f/src/Model.jsx
+++ 46-load-models-with-r3f/src/Model.jsx
@@ -1,12 +1,15 @@
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, Clone } from '@react-three/drei';
 
 useGLTF.preload('./hamburger-draco.glb');
+
 export default function Model() {
-  const hamburger = useGLTF('./hamburger-draco.glb');
+  const model = useGLTF('./hamburger-draco.glb');
 
   return (
     <>
-      <primitive object={hamburger.scene} scale={0.35} />
+      <Clone object={model.scene} scale={0.35} position={[-4, 0, 0]} />
+      <Clone object={model.scene} scale={0.35} position={[0, 0, 0]} />
+      <Clone object={model.scene} scale={0.35} position={[4, 0, 0]} />
     </>
   );
 }
