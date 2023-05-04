6f3963e247a6dce3cd30c1d1899b784c26cf980f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-04 14:50:42 +0300

    switch to burger

--- 46-load-models-with-r3f/src/Model.jsx
+++ 46-load-models-with-r3f/src/Model.jsx
@@ -3,9 +3,9 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
 export default function Model() {
-  const hamburger = useLoader(GLTFLoader, './FlightHelmet/glTF/FlightHelmet.gltf', (loader) => {
+  const hamburger = useLoader(GLTFLoader, './hamburger-draco.glb', (loader) => {
     loader.setDRACOLoader(new DRACOLoader().setDecoderPath('./draco/'));
   });
 
-  return <primitive object={hamburger.scene} scale={5} position={[0, -1, 0]} />;
+  return <primitive object={hamburger.scene} scale={0.35} />;
 }
