cf0e7421e4fbe16043c53a08cb0c359ad4d48edd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-04 16:14:15 +0300

    add animation

--- 46-load-models-with-r3f/src/Fox.jsx
+++ 46-load-models-with-r3f/src/Fox.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { useAnimations } from '@react-three/drei';
 
 useGLTF.preload('./Fox/glTF/Fox.gltf');
 
 export default function Fox(props) {
   const fox = useGLTF('./Fox/glTF/Fox.gltf');
+  const animations = useAnimations(fox.animations, fox.scene);
+
+  useEffect(() => {
+    animations.actions.Survey.play();
+  }, []);
 
   return (
     <>
