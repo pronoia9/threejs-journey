6f7248d19c153e61b89edd8de6060ed4ba6d9910
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-04 15:29:50 +0300

    fix shadow acne

--- 46-load-models-with-r3f/src/Experience.jsx
+++ 46-load-models-with-r3f/src/Experience.jsx
@@ -4,6 +4,7 @@ import { Perf } from 'r3f-perf';
 
 import Model from './Model';
 import ModelPlaceholder from './ModelPlaceholder';
+import Hamburger from './Hamburger';
 
 export default function Experience() {
   return (
@@ -12,7 +13,7 @@ export default function Experience() {
 
       <OrbitControls makeDefault />
 
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={0.04} />
       <ambientLight intensity={0.5} />
 
       <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
@@ -29,7 +30,7 @@ export default function Experience() {
           />
         }
       >
-        <Model />
+        <Hamburger scale={0.35} />
       </Suspense>
     </>
   );
