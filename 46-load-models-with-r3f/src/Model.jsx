f7c8b69128be2ba82529f4b260313da4f1d2e39c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-04 14:52:27 +0300

    use dreis gltf

--- 46-load-models-with-r3f/src/Model.jsx
+++ 46-load-models-with-r3f/src/Model.jsx
@@ -1,11 +1,7 @@
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { useGLTF } from '@react-three/drei';
 
 export default function Model() {
-  const hamburger = useLoader(GLTFLoader, './hamburger-draco.glb', (loader) => {
-    loader.setDRACOLoader(new DRACOLoader().setDecoderPath('./draco/'));
-  });
+  const hamburger = useGLTF('./hamburger-draco.glb');
 
   return <primitive object={hamburger.scene} scale={0.35} />;
 }
