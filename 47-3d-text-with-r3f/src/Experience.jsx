fe7b37dbeea51a09420e10ced79b4eab60e32e3d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-05 16:48:07 +0300

    add matcap to text

--- 47-3d-text-with-r3f/src/Experience.jsx
+++ 47-3d-text-with-r3f/src/Experience.jsx
@@ -1,7 +1,9 @@
-import { OrbitControls, Text3D, Center } from '@react-three/drei';
+import { OrbitControls, Text3D, Center, useMatcapTexture } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
+  const [textMatcap] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
+
   return (
     <>
       <Perf position='top-left' />
@@ -9,9 +11,18 @@ export default function Experience() {
       <OrbitControls makeDefault />
 
       <Center>
-        <Text3D font='./fonts/helvetiker_regular.typeface.json'>
+        <Text3D
+          font='./fonts/helvetiker_regular.typeface.json'
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelSegments={5}
+        >
           Hello R3F
-          <meshNormalMaterial />
+          <meshMatcapMaterial matcap={textMatcap} />
         </Text3D>
       </Center>
     </>
