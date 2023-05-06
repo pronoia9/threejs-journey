5e4bf01af2a90975f17ff745a76496a5ed2bc5df
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-06 17:16:51 +0300

    add pole lights

--- 48-portal-scene-with-r3f/src/Experience.jsx
+++ 48-portal-scene-with-r3f/src/Experience.jsx
@@ -11,9 +11,18 @@ export default function Experience() {
       <OrbitControls makeDefault />
 
       <Center>
+        {/* Scene */}
         <mesh geometry={nodes.baked.geometry}>
           <meshBasicMaterial map={bakedTexture} map-flipY={false} />
         </mesh>
+        {/* Pole Lights */}
+        <mesh geometry={nodes.poleLightA.geometry} position={nodes.poleLightA.position}>
+          <meshBasicMaterial color='#ffffe5' />
+        </mesh>
+        <mesh geometry={nodes.poleLightB.geometry} position={nodes.poleLightB.position}>
+          <meshBasicMaterial color='#ffffe5' />
+        </mesh>
+        {/* Portal */}
       </Center>
     </>
   );
