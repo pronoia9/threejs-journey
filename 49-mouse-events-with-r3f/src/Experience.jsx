950e0876bcfa07bdd2f5c8fcdf4506c3a4654652
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-07 18:23:51 +0300

    use mesh bounds for raycaster on the cube

--- 49-mouse-events-with-r3f/src/Experience.jsx
+++ 49-mouse-events-with-r3f/src/Experience.jsx
@@ -1,11 +1,10 @@
 import { useRef } from 'react';
 import { useFrame, useThree } from '@react-three/fiber'; // TODO check useThree to do the hovering for the Canvas
-import { OrbitControls, useCursor, useGLTF } from '@react-three/drei'; // TODO check useCursor helper instead of hover funcs
+import { OrbitControls, useCursor, useGLTF, meshBounds, useBVH } from '@react-three/drei'; // TODO check useCursor helper instead of hover funcs + check BVH
 
 export default function Experience() {
   const cube = useRef();
   const burger = useGLTF('./hamburger.glb');
-  console.log(burger);
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -33,17 +32,34 @@ export default function Experience() {
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.5} />
 
+      <primitive
+        object={burger.scene}
+        scale={0.25}
+        position={[0, 0.5, 0]}
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log(e.object);
+          console.log(e.eventObject);
+        }}
+      />
+
       <mesh position-x={-2} onPointerEnter={handleHover} onPointerLeave={handleHoverLeave} onClick={handleClick}>
         <sphereGeometry />
         <meshStandardMaterial color='orange' />
       </mesh>
 
-      <mesh ref={cube} position-x={2} scale={1.5} onPointerEnter={handleHover} onPointerLeave={handleHoverLeave} onClick={handleClick}>
+      <mesh
+        ref={cube}
+        raycast={meshBounds}
+        position-x={2}
+        scale={1.5}
+        onPointerEnter={handleHover}
+        onPointerLeave={handleHoverLeave}
+        onClick={handleClick}
+      >
         <boxGeometry />
         <meshStandardMaterial color='mediumpurple' />
       </mesh>
-
-      <primitive object={burger.scene} scale={0.25} position={[0, 0.5, 0]} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
