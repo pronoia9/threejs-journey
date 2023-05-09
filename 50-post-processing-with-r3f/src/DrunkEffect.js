0ca6c0fe9d281891f0e767817a975da870bf1267
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-09 20:15:47 +0300

    add basic mainUv

--- 50-post-processing-with-r3f/src/DrunkEffect.js
+++ 50-post-processing-with-r3f/src/DrunkEffect.js
@@ -1,6 +1,10 @@
 import { Effect } from 'postprocessing';
 
 const fragmentShader = /* glsl */ `
+  void mainUv(inout vec2 uv) {
+    uv.y += sin(uv.x);
+  }
+
   void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
     vec4 color = inputColor;
     color.rgb *= vec3(0.8, 1.0, 0.5);
