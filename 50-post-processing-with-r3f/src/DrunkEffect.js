a877d75a1bac02acffeb50ebe8981aaac0fbbf80
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-09 20:11:43 +0300

    add green hue to effect

--- 50-post-processing-with-r3f/src/DrunkEffect.js
+++ 50-post-processing-with-r3f/src/DrunkEffect.js
@@ -2,7 +2,9 @@ import { Effect } from 'postprocessing';
 
 const fragmentShader = /* glsl */ `
   void mainImage(const in vec4 inputColor, const in vec2 uv, out vec4 outputColor) {
-    outputColor = vec4(uv, 1.0, 1.0);
+    vec4 color = inputColor;
+    color.rgb *= vec3(0.8, 1.0, 0.5);
+    outputColor = color;
   }
 `;
 
