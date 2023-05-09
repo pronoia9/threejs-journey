26064ca494df6c8855394eb14b480ee4dc428847
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-09 18:38:30 +0300

    add ssr effect

--- 50-post-processing-with-r3f/src/Experience.jsx
+++ 50-post-processing-with-r3f/src/Experience.jsx
@@ -1,9 +1,41 @@
 import { OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
-import { EffectComposer, Vignette, Glitch, Noise, Bloom, DepthOfField } from '@react-three/postprocessing';
+import { EffectComposer, Vignette, Glitch, Noise, Bloom, DepthOfField, SSR } from '@react-three/postprocessing';
 import { BlendFunction, GlitchMode } from 'postprocessing';
+import { useControls } from 'leva';
 
 export default function Experience() {
+  const ssrProps = useControls('SSR Effect', {
+    temporalResolve: true,
+    STRETCH_MISSED_RAYS: true,
+    USE_MRT: true,
+    USE_NORMALMAP: true,
+    USE_ROUGHNESSMAP: true,
+    ENABLE_JITTERING: true,
+    ENABLE_BLUR: true,
+    temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+    temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+    maxSamples: { value: 0, min: 0, max: 1 },
+    resolutionScale: { value: 1, min: 0, max: 1 },
+    blurMix: { value: 0.5, min: 0, max: 1 },
+    blurKernelSize: { value: 8, min: 0, max: 8 },
+    blurSharpness: { value: 0.5, min: 0, max: 1 },
+    rayStep: { value: 0.3, min: 0, max: 1 },
+    intensity: { value: 1, min: 0, max: 5 },
+    maxRoughness: { value: 0.1, min: 0, max: 1 },
+    jitter: { value: 0.7, min: 0, max: 5 },
+    jitterSpread: { value: 0.45, min: 0, max: 1 },
+    jitterRough: { value: 0.1, min: 0, max: 1 },
+    roughnessFadeOut: { value: 1, min: 0, max: 1 },
+    rayFadeOut: { value: 0, min: 0, max: 1 },
+    MAX_STEPS: { value: 20, min: 0, max: 20 },
+    NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+    maxDepthDifference: { value: 3, min: 0, max: 10 },
+    maxDepth: { value: 1, min: 0, max: 1 },
+    thickness: { value: 10, min: 0, max: 10 },
+    ior: { value: 1.45, min: 0, max: 2 },
+  });
+
   return (
     <>
       <color args={['#fff']} attach='background' />
@@ -20,11 +52,12 @@ export default function Experience() {
         /> */}
         {/* <Noise premultiply // enables or disables noise premultiplication blendFunction={BlendFunction.OVERLAY} /> */}
         {/* <Bloom mipmapBlur intensity={0.1} luminanceThreshold={0} /> */}
-        <DepthOfField
+        {/* <DepthOfField
           focusDistance={0.025} // where to focus
           focalLength={0.025} // focal length
           bokehScale={6} // bokeh size
-        />
+        /> */}
+        <SSR {...ssrProps} />
       </EffectComposer>
 
       <Perf position='top-left' />
@@ -48,7 +81,8 @@ export default function Experience() {
 
       <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color='greenyellow' />
+        {/* <meshStandardMaterial color='greenyellow' /> */}
+        <meshStandardMaterial color='black' roughness={0} metalness={0} />
       </mesh>
     </>
   );
