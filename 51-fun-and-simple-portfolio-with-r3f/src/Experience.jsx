1bb010d0698db7bf89c05dced8abb659fbb4a392
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-10 22:28:07 +0300

    add presentation controls to leva

--- 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -4,21 +4,30 @@ import { Perf } from 'r3f-perf';
 
 export default function Experience() {
   const pc = useGLTF('./Desk.gltf');
-  // console.log(pc);
+
+  // Scene
   const { scale, position, rotation } = useControls('model', {
     scale: { value: 0.3 },
     position: { value: [0, -0.5, 0] },
     rotation: { value: [-0.1, -0.45, 0] },
   });
 
-  /* sunset, dawn, night, warehouse, forest, apartment, studio, city, park, lobby */
+  // Controls
+  const { polar, azimuth, config, mass, tension, snap } = useControls('presentation controls', {
+    polar: { value: [-0.4, 0.2] },
+    azimuth: { value: [-1, 0.75] },
+    mass: { value: 2 },
+    tension: { value: 200 },
+    snap: { value: true },
+  });
 
   return (
     <>
       <Perf position='top-left' />
       <Environment preset='lobby' />
+      {/* sunset, dawn, night, warehouse, forest, apartment, studio, city, park, lobby */}
       <color args={['#2F2953']} attach='background' />
-      <PresentationControls global rotation={[0.13, 0.1, 0]} polar={[-0.4, 0.2]} azimuth={[-1, 0.75]}>
+      <PresentationControls global rotation={[0.13, 0.1, 0]} polar={polar} azimuth={azimuth} config={{ mass, tension }} snap={snap}>
         <Float rotationIntensity={0.4}>
           <primitive object={pc.scene} scale={scale} position={position} rotation={rotation} />
         </Float>
