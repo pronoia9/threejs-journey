fef8595a230aa5f3c969b2fb1eda4dea6ca45e2b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-10 21:47:50 +0300

    add init scene

--- 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,14 +1,24 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useGLTF, Environment } from '@react-three/drei';
+import { useControls } from 'leva';
 
 export default function Experience() {
+  const pc = useGLTF('./Desk.gltf');
+  // console.log(pc);
+
+  const { scale, position, rotation } = useControls('model', {
+    scale: { value: 0.25 },
+    position: { value: [0, 0, 0] },
+    rotation: { value: [0, 0, 0] },
+  });
+
   return (
     <>
+      <Environment preset='city' />
+      <color args={['#241a1a']} attach='background' />
+
       <OrbitControls makeDefault />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <primitive object={pc.scene} scale={scale} position={position} rotation={rotation} />
     </>
   );
 }
