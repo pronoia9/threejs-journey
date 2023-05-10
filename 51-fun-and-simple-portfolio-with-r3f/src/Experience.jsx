9bfbf7e326bdaa016f1ac0c8abe1fabe4f48cc30
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-10 22:15:24 +0300

    add limitations to presentation controls

--- 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -15,10 +15,10 @@ export default function Experience() {
     <>
       <Perf position='top-left' />
 
-      <Environment preset='city' />
+      <Environment preset='night' />
       <color args={['#2F2953']} attach='background' />
 
-      <PresentationControls global>
+      <PresentationControls global rotation={[0.13, 0.1, 0]} polar={[-0.4, 0.2]} azimuth={[-1, 0.75]}>
         <Float rotationIntensity={0.4}>
           <primitive object={pc.scene} scale={scale} position={position} rotation={rotation} />
         </Float>
