d36318e8c459475dc1031526a250de8a3f8c8739
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-10 21:54:43 +0300

    add perf

--- 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ 51-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,19 +1,22 @@
 import { OrbitControls, useGLTF, Environment, Float } from '@react-three/drei';
 import { useControls } from 'leva';
+import { Perf } from 'r3f-perf';
 
 export default function Experience() {
   const pc = useGLTF('./Desk.gltf');
   // console.log(pc);
   const { scale, position, rotation } = useControls('model', {
     scale: { value: 0.25 },
-    position: { value: [0, 0, 0] },
+    position: { value: [0, -0.5, 0] },
     rotation: { value: [0, 0, 0] },
   });
 
   return (
     <>
+      <Perf position='top-left' />
+
       <Environment preset='city' />
-      <color args={['#241a1a']} attach='background' />
+      <color args={['#2F2953']} attach='background' />
 
       <OrbitControls makeDefault />
 
