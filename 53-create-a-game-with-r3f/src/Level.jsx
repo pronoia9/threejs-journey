f9488f0d858f055b1451287c0d7999cd2fcce16e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-18 23:48:18 +0300

    add initial block limbo

--- 53-create-a-game-with-r3f/src/Level.jsx
+++ 53-create-a-game-with-r3f/src/Level.jsx
@@ -15,8 +15,9 @@ const wallMaterial = new MeshStandardMaterial({ color: 'slategrey' });
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 4]} />
-      <BlockSpinner position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 8]} />
+      <BlockSpinner position={[0, 0, 4]} />
+      <BlockLimbo position={[0, 0, 0]} />
     </>
   );
 }
@@ -41,6 +42,29 @@ function BlockSpinner({ position = [0, 0, 0] }) {
     const rotation = new Quaternion();
     rotation.setFromEuler(new Euler(0, time * spin, 0));
     obstacleRef.current.setNextKinematicRotation(rotation);
+  });
+
+  return (
+    <group position={position}>
+      {/* Floor */}
+      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} material={floor2Material} receiveShadow />
+      {/* Obstacle */}
+      <RigidBody ref={obstacleRef} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+        <mesh geometry={boxGeometry} position={[0, 0, 0]} scale={[3.5, 0.3, 0.3]} material={obstacleMaterial} castShadow receiveShadow />
+      </RigidBody>
+    </group>
+  );
+}
+
+function BlockLimbo({ position = [0, 0, 0] }) {
+  const [spin] = useState((Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1));
+  const obstacleRef = useRef();
+
+  // Animation
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const y = Math.sin(time) + 1.15;
+    obstacleRef.current.setNextKinematicTranslation({ x: 0, y, z: 0 });
   });
 
   return (
