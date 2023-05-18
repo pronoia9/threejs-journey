e8cdd791c07aec6bf03e33782397fa4810625e5d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 01:18:18 +0300

    finish bounds

--- 53-create-a-game-with-r3f/src/components/Bounds.jsx
+++ 53-create-a-game-with-r3f/src/components/Bounds.jsx
@@ -1,10 +1,15 @@
 import { geometries, materials } from './utils';
 const { wall } = materials;
 
-export default function BlockStart({ length = 1, geometry = geometries.box, materials }) {
+export default function BlockStart({ length = 1, geometry = geometries.box, material }) {
   return (
     <group position={[0, 0, 0]}>
-      <mesh geometry={geometry} scale={[4, 0.2, 4]} material={materials?.floor || wall} receiveShadow />
+      {/* Right Wall */}
+      <mesh geometry={geometry} position={[2.15, 0.75, -(length * 4 / 2) + (4 / 2)]} scale={[0.3, 1.9, length * 4]} material={material || wall} castShadow />
+      {/* Left Wall */}
+      <mesh geometry={geometry} position={[-2.15, 0.75, -(length * 4 / 2) + (4 / 2)]} scale={[0.3, 1.9, length * 4]} material={material || wall} receiveShadow />
+      {/* End Wall */}
+      <mesh geometry={geometry} position={[0, 0.75, -(length * 4) + (4 / 2)]} scale={[4.6, 1.9, 0.3]} material={material || wall} receiveShadow />
     </group>
   );
 }
