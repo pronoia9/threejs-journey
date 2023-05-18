67ccb9111a35c67de4c5b19b65b86ec22d482b34
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 01:19:29 +0300

    add rigid body

--- 53-create-a-game-with-r3f/src/components/Bounds.jsx
+++ 53-create-a-game-with-r3f/src/components/Bounds.jsx
@@ -1,15 +1,17 @@
+import { RigidBody } from '@react-three/rapier';
+
 import { geometries, materials } from './utils';
 const { wall } = materials;
 
 export default function BlockStart({ length = 1, geometry = geometries.box, material }) {
   return (
-    <group position={[0, 0, 0]}>
+    <RigidBody type='fixed' restitution={0.2} friction={0}>
       {/* Right Wall */}
       <mesh geometry={geometry} position={[2.15, 0.75, -(length * 4 / 2) + (4 / 2)]} scale={[0.3, 1.9, length * 4]} material={material || wall} castShadow />
       {/* Left Wall */}
       <mesh geometry={geometry} position={[-2.15, 0.75, -(length * 4 / 2) + (4 / 2)]} scale={[0.3, 1.9, length * 4]} material={material || wall} receiveShadow />
       {/* End Wall */}
       <mesh geometry={geometry} position={[0, 0.75, -(length * 4) + (4 / 2)]} scale={[4.6, 1.9, 0.3]} material={material || wall} receiveShadow />
-    </group>
+    </RigidBody>
   );
 }
