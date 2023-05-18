c8f5ae09d4773a4dfc19485726a8fe16fed1d53a
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-18 23:59:31 +0300

    finish block axe

--- 53-create-a-game-with-r3f/src/Level.jsx
+++ 53-create-a-game-with-r3f/src/Level.jsx
@@ -15,9 +15,10 @@ const wallMaterial = new MeshStandardMaterial({ color: 'slategrey' });
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 8]} />
-      <BlockSpinner position={[0, 0, 4]} />
-      <BlockLimbo position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 12]} />
+      <BlockSpinner position={[0, 0, 8]} />
+      <BlockLimbo position={[0, 0, 4]} />
+      <BlockAxe position={[0, 0, 0]} />
     </>
   );
 }
@@ -74,6 +75,29 @@ function BlockLimbo({ position = [0, 0, 0] }) {
       {/* Obstacle */}
       <RigidBody ref={obstacleRef} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0}>
         <mesh geometry={boxGeometry} position={[0, 0, 0]} scale={[3.5, 0.3, 0.3]} material={obstacleMaterial} castShadow receiveShadow />
+      </RigidBody>
+    </group>
+  );
+}
+
+function BlockAxe({ position = [0, 0, 0] }) {
+  const [offset] = useState(Math.random() * Math.PI * 2);
+  const obstacleRef = useRef();
+
+  // Animation
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    const x = Math.sin(time + offset) * 1.25;
+    obstacleRef.current.setNextKinematicTranslation({ x: x, y: position[1] + 0.75, z: position[2] });
+  });
+
+  return (
+    <group position={position}>
+      {/* Floor */}
+      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} material={floor2Material} receiveShadow />
+      {/* Obstacle */}
+      <RigidBody ref={obstacleRef} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+        <mesh geometry={boxGeometry} position={[0, 0, 0]} scale={[1.5, 1.5, 0.3]} material={obstacleMaterial} castShadow receiveShadow />
       </RigidBody>
     </group>
   );
