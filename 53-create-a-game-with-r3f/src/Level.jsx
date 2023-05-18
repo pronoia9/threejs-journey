4329ed425b38e974dd81613887fd01c9efd220a4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-18 23:39:23 +0300

    add init block spiner + rigidbody + rotation

--- 53-create-a-game-with-r3f/src/Level.jsx
+++ 53-create-a-game-with-r3f/src/Level.jsx
@@ -1,4 +1,7 @@
-import { BoxGeometry, ColorManagement, MeshStandardMaterial } from 'three';
+import { useRef } from 'react';
+import { BoxGeometry, ColorManagement, Euler, MeshStandardMaterial, Quaternion } from 'three';
+import { useFrame } from '@react-three/fiber';
+import { RigidBody } from '@react-three/rapier';
 
 // Fix Colors
 ColorManagement.legacyMode = false;
@@ -9,19 +12,44 @@ const floor2Material = new MeshStandardMaterial({ color: 'greenyellow' });
 const obstacleMaterial = new MeshStandardMaterial({ color: 'orangered' });
 const wallMaterial = new MeshStandardMaterial({ color: 'slategrey' });
 
-
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 0]} scale={[4, 0.2, 4]} material={floor1Material} />
+      <BlockStart position={[0, 0, 4]} />
+      <BlockSpinner position={[0, 0, 0]} />
     </>
   );
 }
 
-function BlockStart({ position = [0, 0, 0], scale, material }) {
+function BlockStart({ position = [0, 0, 0] }) {
   return (
     <group position={position}>
-      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={scale} material={material} receiveShadow />
+      {/* Floor */}
+      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} material={floor1Material} receiveShadow />
+    </group>
+  );
+}
+
+function BlockSpinner({ position = [0, 0, 0] }) {
+  const obstacleRef = useRef();
+
+  // Animation
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime();
+    // Rotation of the obstacle
+    const rotation = new Quaternion();
+    rotation.setFromEuler(new Euler(0, time, 0));
+    obstacleRef.current.setNextKinematicRotation(rotation);
+  });
+
+  return (
+    <group position={position}>
+      {/* Floor */}
+      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} material={floor2Material} receiveShadow />
+      {/* Obstacle */}
+      <RigidBody ref={obstacleRef} type='kinematicPosition' position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+        <mesh geometry={boxGeometry} position={[0, 0, 0]} scale={[3.5, 0.3, 0.3]} material={obstacleMaterial} castShadow receiveShadow />
+      </RigidBody>
     </group>
   );
 }
