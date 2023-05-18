6e771b2e2b3dcc1ad8c6229f5af9c5defcf95b38
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 00:59:38 +0300

    add blocks array + map it

--- 53-create-a-game-with-r3f/src/components/Level.jsx
+++ 53-create-a-game-with-r3f/src/components/Level.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { BlockStart, BlockSpinner, BlockLimbo, BlockAxe, BlockEnd } from './';
 import { geometries, materials } from './utils';
 
@@ -5,9 +6,15 @@ const { box } = geometries;
 const { floor1, floor2, obstacle, wall } = materials;
 
 export default function Level({ count = 5, types = [BlockSpinner, BlockLimbo, BlockAxe] }) {
+  // Generate a blocks array filled with random items from types
+  const blocks = useMemo(() => new Array(count).fill().map(() => types[Math.floor(Math.random() * types.length)]), [count, types]);
+
   return (
     <>
-      <BlockStart position={[0, 0, 0]} geometry={box} materials={{ floor: floor1 }} />
+      <BlockStart position={[0, 0, 0]} />
+      {blocks.map((Block, index) => (
+        <Block key={`block-#${index}`} position={[0, 0, -index * 4 - 4]} />
+      ))}
     </>
   );
 }
