e232e3c19cd49a7e9c7ba9baa93dac08791b58d6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-18 22:56:37 +0300

    add materials

--- 53-create-a-game-with-r3f/src/Level.jsx
+++ 53-create-a-game-with-r3f/src/Level.jsx
@@ -1,21 +1,23 @@
-import { BoxGeometry } from 'three';
+import { BoxGeometry, MeshStandardMaterial } from 'three';
 
 const boxGeometry = new BoxGeometry(1, 1, 1);
+const floor1Material = new MeshStandardMaterial({ color: 'limegreen' });
+const floor2Material = new MeshStandardMaterial({ color: 'greenyellow' });
+const obstacleMaterial = new MeshStandardMaterial({ color: 'orangered' });
+const wallMaterial = new MeshStandardMaterial({ color: 'slategrey' });
 
 export default function Level() {
   return (
     <>
-      <BlockStart position={[0, 0, 0]} />
+      <BlockStart position={[0, 0, 0]} scale={[4, 0.2, 4]} material={floor1Material} />
     </>
   );
 }
 
-function BlockStart({ position = [0, 0, 0] }) {
+function BlockStart({ position = [0, 0, 0], scale, material }) {
   return (
     <group position={position}>
-      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow>
-        <meshStandardMaterial color='limegreen' />
-      </mesh>
+      <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={scale} receiveShadow material={material} />
     </group>
   );
 }
