38e5b2844caedcc9be4e48b2b04620c446e32302
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 01:23:14 +0300

    add floor bounds

--- 53-create-a-game-with-r3f/src/components/Bounds.jsx
+++ 53-create-a-game-with-r3f/src/components/Bounds.jsx
@@ -1,4 +1,4 @@
-import { RigidBody } from '@react-three/rapier';
+import { CuboidCollider, RigidBody } from '@react-three/rapier';
 
 import { geometries, materials } from './utils';
 const { wall } = materials;
@@ -7,11 +7,25 @@ export default function BlockStart({ length = 1, geometry = geometries.box, mate
   return (
     <RigidBody type='fixed' restitution={0.2} friction={0}>
       {/* Right Wall */}
-      <mesh geometry={geometry} position={[2.15, 0.75, -(length * 4 / 2) + (4 / 2)]} scale={[0.3, 1.9, length * 4]} material={material || wall} castShadow />
+      <mesh
+        geometry={geometry}
+        position={[2.15, 0.75, -((length * 4) / 2) + 4 / 2]}
+        scale={[0.3, 1.9, length * 4]}
+        material={material || wall}
+        castShadow
+      />
       {/* Left Wall */}
-      <mesh geometry={geometry} position={[-2.15, 0.75, -(length * 4 / 2) + (4 / 2)]} scale={[0.3, 1.9, length * 4]} material={material || wall} receiveShadow />
+      <mesh
+        geometry={geometry}
+        position={[-2.15, 0.75, -((length * 4) / 2) + 4 / 2]}
+        scale={[0.3, 1.9, length * 4]}
+        material={material || wall}
+        receiveShadow
+      />
       {/* End Wall */}
-      <mesh geometry={geometry} position={[0, 0.75, -(length * 4) + (4 / 2)]} scale={[4.6, 1.9, 0.3]} material={material || wall} receiveShadow />
+      <mesh geometry={geometry} position={[0, 0.75, -(length * 4) + 4 / 2]} scale={[4.6, 1.9, 0.3]} material={material || wall} receiveShadow />
+      {/* Floor */}
+      <CuboidCollider args={[2, 0.1, 2 * length]} position={[0, -0.1, -((length * 4) / 2) + 4 / 2]} />
     </RigidBody>
   );
 }
