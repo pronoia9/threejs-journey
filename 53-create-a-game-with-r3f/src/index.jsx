50d69103e584150df6207b1ee13483531bad24c9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 20:04:41 +0300

    add keyboard controls

--- 53-create-a-game-with-r3f/src/index.jsx
+++ 53-create-a-game-with-r3f/src/index.jsx
@@ -1,13 +1,24 @@
 import './style.css';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
+import { KeyboardControls } from '@react-three/drei';
 
 import Experience from './Experience.jsx';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-  <Canvas shadows camera={{ fov: 45, near: 0.1, far: 200, position: [2.5, 4, 6] }}>
-    <Experience />
-  </Canvas>
+  <KeyboardControls
+    map={[
+      { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+      { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+      { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+      { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+      { name: 'jump', keys: ['Space'] },
+    ]}
+  >
+    <Canvas shadows camera={{ fov: 45, near: 0.1, far: 200, position: [2.5, 4, 6] }}>
+      <Experience />
+    </Canvas>
+  </KeyboardControls>
 );
