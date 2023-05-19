34f7ef36cea2a404768a3e81f2d72933d4634ce8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 00:55:42 +0300

    add subscription to store

--- 53-create-a-game-with-r3f/src/stores/useGame.js
+++ 53-create-a-game-with-r3f/src/stores/useGame.js
@@ -1,24 +1,27 @@
 import { create } from 'zustand';
+import { subscribeWithSelector } from 'zustand/middleware';
 
-export default create((set) => {
-  return {
-    blocksCount: 3,
-    // Phases
-    phase: 'ready',
-    start: () => {
-      set((state) => {
-        return state.phase !== 'playing' ? { phase: 'playing' } : {};
-      });
-    },
-    end: () => {
-      set((state) => {
-        return state.phase === 'playing' ? { phase: 'ended' } : {};
-      });
-    },
-    restart: () => {
-      set((state) => {
-        return state.phase !== 'ready' ? { phase: 'ready' } : {};
-      });
-    },
-  };
-});
+export default create(
+  subscribeWithSelector((set) => {
+    return {
+      blocksCount: 3,
+      // Phases
+      phase: 'ready',
+      start: () => {
+        set((state) => {
+          return state.phase !== 'playing' ? { phase: 'playing' } : {};
+        });
+      },
+      end: () => {
+        set((state) => {
+          return state.phase === 'playing' ? { phase: 'ended' } : {};
+        });
+      },
+      restart: () => {
+        set((state) => {
+          return state.phase !== 'ready' ? { phase: 'ready' } : {};
+        });
+      },
+    };
+  })
+);
