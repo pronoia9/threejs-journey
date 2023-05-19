10a00341d38e0f75449ccc54628b24a1170cb04e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 20:41:36 +0300

    init impulse & torque

--- 53-create-a-game-with-r3f/src/components/Player.jsx
+++ 53-create-a-game-with-r3f/src/components/Player.jsx
@@ -1,8 +1,26 @@
+import { useKeyboardControls } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import { RigidBody } from '@react-three/rapier';
+import { useRef } from 'react';
 
 export default function Player() {
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const bodyRef = useRef();
+
+  useFrame((state, delta) => {
+    const { forward, rightward, backward, leftward } = getKeys();
+    
+    const impulse = { x: 0, y: 0, z: 0 }, torque = { x: 0, y: 0, z: 0 };
+    const impulseStrength = 1 * delta, torqueStrength = 1 * delta;
+
+    if (forward) impulse.z -= impulseStrength;
+
+    bodyRef.current.applyImpulse(impulse);
+    bodyRef.current.applyTorqueImpulse(torque);
+  })
+
   return (
-    <RigidBody colliders='ball' restitution={0.2} friction={1} position={[0, 1, 0]}>
+    <RigidBody ref={bodyRef} colliders='ball' restitution={0.2} friction={1} position={[0, 1, 0]}>
       <mesh castShadow>
         <icosahedronGeometry args={[0.3, 1]} />
         <meshStandardMaterial flatShading color='mediumpurple' />
