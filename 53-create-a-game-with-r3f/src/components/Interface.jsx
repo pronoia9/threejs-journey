112f34c2df44a76c4d886c040cfb7234f1921023
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 01:37:40 +0300

    add restart on click

--- 53-create-a-game-with-r3f/src/components/Interface.jsx
+++ 53-create-a-game-with-r3f/src/components/Interface.jsx
@@ -1,18 +1,24 @@
 import { useKeyboardControls } from '@react-three/drei';
 
+import useGame from '../stores/useGame';
+
 export default function Interface() {
+  // Keys
   const forward = useKeyboardControls((state) => state.forward),
     rightward = useKeyboardControls((state) => state.rightward),
     backward = useKeyboardControls((state) => state.backward),
     leftward = useKeyboardControls((state) => state.leftward),
     space = useKeyboardControls((state) => state.space);
+  // Store
+  const phase = useGame((state) => state.phase),
+    restart = useGame((state) => state.restart);
 
   return (
     <div className='interface'>
       {/* Time */}
       <div className='time'>0.00</div>
       {/* Restart */}
-      <div className='restart'>Restart</div>
+      {phase === 'ended' && <div className='restart' onClick={restart}>Restart</div>}
       {/* Controls */}
       <div className='controls'>
         <div className='raw'>
