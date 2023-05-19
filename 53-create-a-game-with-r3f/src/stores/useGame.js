d9188d2bf5fac82f43290e24a23310a53be89c84
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 00:21:01 +0300

    fix phase function returns

--- 53-create-a-game-with-r3f/src/stores/useGame.js
+++ 53-create-a-game-with-r3f/src/stores/useGame.js
@@ -1,7 +1,12 @@
 import { create } from 'zustand';
 
-export default create(() => {
+export default create((set) => {
   return {
     blocksCount: 3,
+    // Phases
+    phase: 'ready',
+    start: () => { set(() => { return { phase: 'playing' }; }); },
+    end: () => { set(() => { return { phase: 'ended' }; }); },
+    restart: () => { set(() => { return { phase: 'ready' }; }); },
   };
 });
