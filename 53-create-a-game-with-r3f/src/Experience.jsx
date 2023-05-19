e8cb444c4a44c99786a0433ce32b7ad880cff09e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 00:05:31 +0300

    get blocks count from store and send to level

--- 53-create-a-game-with-r3f/src/Experience.jsx
+++ 53-create-a-game-with-r3f/src/Experience.jsx
@@ -1,14 +1,17 @@
 import { Physics, Debug } from '@react-three/rapier';
 
 import { Lights, Level, Player } from './components';
+import useGame from './stores/useGame';
 
 export default function Experience() {
+  const blocksCount = useGame((state) => state.blocksCount);
+
   return (
     <>
       <Physics>
         <Debug />
         <Lights />
-        <Level />
+        <Level blocksCount={blocksCount} />
         <Player />
       </Physics>
     </>
