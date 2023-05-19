b2b58ee9d161a6b5b82018638b7761c7c0e46a57
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 20:51:06 +0300

    add damping

--- 53-create-a-game-with-r3f/src/components/Player.jsx
+++ 53-create-a-game-with-r3f/src/components/Player.jsx
@@ -25,7 +25,7 @@ export default function Player() {
   })
 
   return (
-    <RigidBody ref={bodyRef} colliders='ball' restitution={0.2} friction={1} position={[0, 1, 0]}>
+    <RigidBody ref={bodyRef} colliders='ball' restitution={0.2} friction={1} linearDamping={0.5} angularDamping={0.5} position={[0, 1, 0]}>
       <mesh castShadow>
         <icosahedronGeometry args={[0.3, 1]} />
         <meshStandardMaterial flatShading color='mediumpurple' />
