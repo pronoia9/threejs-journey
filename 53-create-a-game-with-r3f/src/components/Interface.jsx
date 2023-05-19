4ab42fa1f8f6b3a18bb8a39d425b889fb7a7ac11
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-19 23:08:18 +0300

    add init interface elements

--- 53-create-a-game-with-r3f/src/components/Interface.jsx
+++ 53-create-a-game-with-r3f/src/components/Interface.jsx
@@ -1,3 +1,24 @@
 export default function Interface() {
-  return <div>Interface</div>;
+  return (
+    <div className='interface'>
+      {/* Time */}
+      <div className='time'>0.00</div>
+      {/* Restart */}
+      <div className='restart'>Restart</div>
+      {/* Controls */}
+      <div className='controls'>
+        <div className='raw'>
+          <div className='key'></div>
+        </div>
+        <div className='raw'>
+          <div className='key'></div>
+          <div className='key'></div>
+          <div className='key'></div>
+        </div>
+        <div className='raw'>
+          <div className='key large'></div>
+        </div>
+      </div>
+    </div>
+  );
 }
