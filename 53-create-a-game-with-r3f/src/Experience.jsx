b514f09032655c1e5fd6e3fef731705e63cfea6e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 02:43:49 +0300

    add seed and send to level as prop

--- 53-create-a-game-with-r3f/src/Experience.jsx
+++ 53-create-a-game-with-r3f/src/Experience.jsx
@@ -5,13 +5,14 @@ import useGame from './stores/useGame';
 
 export default function Experience() {
   const blocksCount = useGame((state) => state.blocksCount);
+  const blocksSeed = useGame((state) => state.blocksSeed);
 
   return (
     <>
       <Physics>
         <Debug />
         <Lights />
-        <Level blocksCount={blocksCount} />
+        <Level blocksCount={blocksCount} seed={blocksSeed} />
         <Player />
       </Physics>
     </>
