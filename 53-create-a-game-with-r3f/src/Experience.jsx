992a33cb7c9cbb9882fdd61ddb580d88ac74722c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 03:12:37 +0300

    add effects

--- 53-create-a-game-with-r3f/src/Experience.jsx
+++ 53-create-a-game-with-r3f/src/Experience.jsx
@@ -1,6 +1,6 @@
 import { Physics, Debug } from '@react-three/rapier';
 
-import { Lights, Level, Player } from './components';
+import { Lights, Level, Player, Effects } from './components';
 import useGame from './stores/useGame';
 
 export default function Experience() {
@@ -12,10 +12,11 @@ export default function Experience() {
       <color args={['#7CD8FA']} attach='background' />
       <Physics>
         <Debug />
-        <Lights />
         <Level blocksCount={blocksCount} seed={blocksSeed} />
         <Player />
       </Physics>
+      <Lights />
+      <Effects />
     </>
   );
 }
