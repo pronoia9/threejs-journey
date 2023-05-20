b22c564bf50e4f5e757d50a8bb64c9f57b6d4331
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 03:06:57 +0300

    add floating text

--- 53-create-a-game-with-r3f/src/components/BlockEnd.jsx
+++ 53-create-a-game-with-r3f/src/components/BlockEnd.jsx
@@ -1,4 +1,4 @@
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, Float, Text } from '@react-three/drei';
 import { RigidBody } from '@react-three/rapier';
 
 import { geometries, materials } from './utils';
@@ -18,6 +18,13 @@ export default function BlockEnd({ position = [0, 0, 0], geometry = geometries.b
       <RigidBody type='fixed' colliders='hull' position={[0, 0.25, 0]} restitution={0.2} friction={0}>
         <primitive object={hamburger.scene} scale={0.2} />
       </RigidBody>
+      {/* Text */}
+      <Float floatIntensity={0.25} rotationIntensity={0.25}>
+        <Text font='/bebas-neue-v9-latin-regular.woff' scale={0.8} position={[0, 2.25, 2]}>
+          FINISH
+          <meshBasicMaterial toneMapped={false} />
+        </Text>
+      </Float>
     </group>
   );
 }
