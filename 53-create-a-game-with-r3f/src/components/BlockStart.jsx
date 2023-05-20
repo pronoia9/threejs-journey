15b534e7ba62884b56b3a92cab7123ad40bc5e56
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-05-20 03:02:40 +0300

    add floating text

--- 53-create-a-game-with-r3f/src/components/BlockStart.jsx
+++ 53-create-a-game-with-r3f/src/components/BlockStart.jsx
@@ -1,4 +1,5 @@
-import { geometries, materials } from "./utils";
+import { Float, Text } from '@react-three/drei';
+import { geometries, materials } from './utils';
 const { floor1 } = materials;
 
 export default function BlockStart({ position = [0, 0, 0], geometry = geometries.box, material = floor1 }) {
@@ -6,6 +7,21 @@ export default function BlockStart({ position = [0, 0, 0], geometry = geometries
     <group position={position}>
       {/* Floor */}
       <mesh geometry={geometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} material={material} receiveShadow />
+      {/* Text */}
+      <Float floatIntensity={0.25} rotationIntensity={0.25}>
+        <Text
+          font='/bebas-neue-v9-latin-regular.woff'
+          scale={0.4}
+          maxWidth={0.25}
+          lineHeight={0.75}
+          textAlign='right'
+          position={[0.75, 0.65, 0]}
+          rotation={[0, -0.25, 0]}
+        >
+          Marble Race
+          <meshBasicMaterial toneMapped={false} />
+        </Text>
+      </Float>
     </group>
   );
 }
